b2b0de8c62f870973c64b5a28440d7dc78f80a97
ryanlightley <rlightley88@gmail.com>
2024-12-06 18:31:56 +0000

    update

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -10,13 +10,13 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<C-l>', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>e'] = 'close_window',
           ['l'] = 'open',
           ['h'] = 'close_node',
           ['H'] = 'toggle_hidden',
